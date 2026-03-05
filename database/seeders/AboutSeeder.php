b4dfcb8c9e80428366b9c8889507c22d901547ac
Murad <agamedov94@mail.ru>
2026-03-05 21:13:23 +0400

    correct6 media in service

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -20,14 +20,14 @@ class AboutSeeder extends Seeder
             "image" => "header_logo_light_for_mode.svg",
             "translations" => [
                 "image_alt_text" => [
-                    3 => "image_alt_az",
-                    4 => "image_alt_en",
-                    5 => "image_alt_ru"
+                    1 => "image_alt_az",
+                    2 => "image_alt_en",
+                    3 => "image_alt_ru"
                 ],
                 "text" => [
-                    3 => "text_az",
-                    4 => "text_en",
-                    5 => "text_ru"
+                    1 => "text_az",
+                    2 => "text_en",
+                    3 => "text_ru"
                 ]
             ]
 
@@ -38,7 +38,7 @@ class AboutSeeder extends Seeder
             "image" => $datas["image"]
         ]);
 
-        foreach([3, 4, 5] as $languageId) {
+        foreach([1, 2, 3] as $languageId) {
             AboutTranslation::create([
                 "image_alt_text" => $datas["translations"]["image_alt_text"][$languageId],
                 "text" => $datas["translations"]["text"][$languageId],
@@ -46,6 +46,6 @@ class AboutSeeder extends Seeder
                 "about_id" => $about->id
             ]);
         }
-        
+
     }
 }
