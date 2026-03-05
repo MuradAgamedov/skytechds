6947f16e3f0e47622e07af336b83bf453142af9d
Murad <agamedov94@mail.ru>
2026-03-05 20:26:40 +0400

    correct6 media in service

--- app/Observers/BlogCategoryObserver.php
+++ app/Observers/BlogCategoryObserver.php
@@ -9,13 +9,5 @@ class BlogCategoryObserver
     public function creating(BlogCategory $blogCategory)
     {
         $blogCategory->order = BlogCategory::lockForUpdate()->max("order") + 1;
-        $blogCategory->slug = $blogCategory->slug ?? str()->slug($blogCategory->translations()->where("language_id", 1)->first()->title) . "-" . uniqid();
-    }
-
-    public function updating(BlogCategory $blogCategory)
-    {
-        if (!$blogCategory->slug) {
-            $blogCategory->slug = str()->slug($blogCategory->translations()->where("language_id", 1)->first()->title) . "-" . uniqid();
-        }
     }
 }
--- app/Observers/ServiceObserver.php
+++ app/Observers/ServiceObserver.php
@@ -7,13 +7,5 @@ class ServiceObserver
     public function creating(Service $service)
     {
         $service->order = (Service::lockForUpdate()->max("order") ?? 0) + 1;
-        $service->slug = $service->slug ?? str()->slug($service->translations()->where("language_id", 1)->first()->title) . "-" . uniqid();
-    }
-
-    public function updating(Service $service)
-    {
-        if (!$service->slug) {
-            $service->slug = str()->slug($service->translations()->where("language_id", 1)->first()->title) . "-" . uniqid();
-        }
     }
 }
