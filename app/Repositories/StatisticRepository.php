2f8aadb1b80a80502e2b89643303d455c7bfc709
Murad Agamedov <agamedov94@mail.ru>
2026-03-06 10:47:12 +0400

    commit

--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -11,12 +11,14 @@ use App\Helpers\DB\WithTranslation\DeleteHelper;
 use App\Helpers\DB\WithTranslation\FindHelper;
 use App\Helpers\DB\WithTranslation\ReadHelper;
 use App\Helpers\DB\WithTranslation\UpdateHelper;
+use App\Support\ImageService;
 
 class StatisticRepository implements StatisticRepositoryInterface
 {
     use CreateHelper, ReadHelper, UpdateHelper, DeleteHelper, FindHelper;
-    public function __construct(public Statistic $model, public StatisticTranslation $translationModel, public LanguageService $languageService) {
+    public function __construct(public Statistic $model, public StatisticTranslation $translationModel, public LanguageService $languageService, public ImageService $image_service) {
         $this->translationRelationField = "statistic_id";
+        $this->folderName = "statistics";
     }
 
 }
