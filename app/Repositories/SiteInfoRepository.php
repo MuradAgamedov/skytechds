cd115712c48389779cb4a1a734b1465074ca29b6
Murad <agamedov94@mail.ru>
2026-03-07 13:29:45 +0400

    commit

--- app/Repositories/SiteInfoRepository.php
+++ app/Repositories/SiteInfoRepository.php
@@ -9,12 +9,12 @@ use App\Helpers\DB\WithTranslation\UpdateHelper;
 use App\Interfaces\Repositories\SiteInfoRepositoryInterface;
 use App\Models\SiteInfo\SiteInfo;
 use App\Models\SiteInfo\SiteInfoTranslation;
-
+use App\Support\ImageService;
 
 class SiteInfoRepository implements SiteInfoRepositoryInterface
 {
     use UpdateHelper, ReadHelper, FirstHelper, FindHelper;
-    public function __construct(public SiteInfo  $model, public SiteInfoTranslation $translationModel) {
+    public function __construct(public SiteInfo  $model, public SiteInfoTranslation $translationModel, public ImageService $image_service) {
         $this->folderName = "site_infos";
         $this->translationRelationField = "site_info_id";
     }
